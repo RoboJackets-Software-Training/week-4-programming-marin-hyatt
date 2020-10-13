25e904c08a485dad7c02660cc64d32c5481ca67d
vagrant <mhyatt7@gatech.edu>
2020-10-13 19:26:36 +0000

    Exercise 4_1 and exercise 4_2 done

--- week_4_exercises/exercise_4_1/your_node.cpp
+++ week_4_exercises/exercise_4_1/your_node.cpp
@@ -1,10 +1,14 @@
 #include <ros/ros.h>
 
 // -= (EXERCISE 1) add the include for the string message! =-
+#include <std_msgs/String.h>
 
 // -========================================================-
 
 // -= (EXERCISE 1) your callback function should be defined here! =-
+void callback_function (std_msgs::String std_msg) {
+	ROS_INFO_STREAM(std_msg.data);
+}
 
 // -===============================================================-
 
@@ -13,6 +17,7 @@ int main(int argc, char** argv){
     ros::NodeHandle p_nh = ros::NodeHandle("~");
 
     // -= (EXERCISE 1) create the ros subscriber here, and have it use your callback function! =-
+    ros::Subscriber subscriber = p.nh.subscribe("/string.topic", 1, &callback_function);
 
     // -========================================================================================-
 
